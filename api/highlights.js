c205e286768b5538e663854bf3f3b07c610cc330
goalguide-mm <mmmmc693363720@gmail.com>
2026-01-31 22:37:45 +0630

    Update highlights.js

--- api/highlights.js
+++ api/highlights.js
@@ -1,8 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const r = await fetch(
-      "https://www.scorebat.com/video-api/v3/feed/?token=MTY4OTdfMTcwNjY3Mzk3NF9mMDJiZTU3YmM0MzU0YWRiOTViYmI3ODU0YjE3Y2M1Yg=="
-    );
+    const r = await fetch("https://www.scorebat.com/video-api/v3/free-feed/");
     const data = await r.json();
 
     res.setHeader("Access-Control-Allow-Origin", "*");

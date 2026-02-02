5906faed7c40ed3d3afc6805d5053b281a618e9b
goalguide-mm <mmmmc693363720@gmail.com>
2026-02-02 12:41:56 +0630

    Update live.js

--- api/live.js
+++ api/live.js
@@ -1,19 +1,14 @@
 export default async function handler(req, res) {
   const API_KEY = process.env.ALLSPORTS_API_KEY;
 
-  if (!API_KEY) {
-    return res.status(500).json({ error: "API KEY missing" });
-  }
-
-  const url =
-    "https://apiv2.allsportsapi.com/football/?met=Livescore&APIkey=" +
-    API_KEY;
+  const url = `https://apiv2.allsportsapi.com/football/?met=Livescore&APIkey=${API_KEY}`;
 
   try {
     const r = await fetch(url);
     const data = await r.json();
+
     res.status(200).json(data.result || []);
   } catch (e) {
-    res.status(500).json({ error: "Failed to load live matches" });
+    res.status(500).json([]);
   }
 }

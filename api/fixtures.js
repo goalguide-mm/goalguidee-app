ee927850d01b798efd050dc49f6078d41c427d00
goalguide-mm <mmmmc693363720@gmail.com>
2026-02-04 15:01:04 +0630

    Update fixtures.js

--- api/fixtures.js
+++ api/fixtures.js
@@ -1,17 +1,12 @@
-export const matches = [
-  {
-    id: 12345,
-    league: "Premier League",
-    leagueLogo: "https://upload.wikimedia.org/wikipedia/en/f/f2/Premier_League_Logo.svg",
-
-    home: "Arsenal",
-    homeLogo: "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg",
-
-    away: "Chelsea",
-    awayLogo: "https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg",
-
-    status: "NS",
-    homeScore: 0,
-    awayScore: 0
-  }
-];
+eapp.get("/api/fixtures/today", (req, res) => {
+  res.json([
+    {
+      id: 12345,
+      league: "Premier League",
+      home: "Arsenal",
+      away: "Chelsea",
+      homeLogo: "https://...",
+      awayLogo: "https://..."
+    }
+  ]);
+});
